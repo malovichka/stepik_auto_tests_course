9c766eba61e40d513e6569ad3bcb19a8d7444484
malovichka <malovichko1911@gmail.com>
2023-09-20 09:51:50 +0300

    made changes in conftest to have the ability to pick browser

--- part_3/conftest.py
+++ part_3/conftest.py
@@ -1,13 +1,26 @@
 import pytest
 from selenium import webdriver
 
+def pytest_addoption(parser):
+    parser.addoption('--browser_name', action = 'store', default='chrome', 
+                     help='Choose browser: chrome or firefox')
+
+
 @pytest.fixture(scope='function')
-def browser() -> webdriver.Chrome:
-    print('\nstart browser for testing...')
-    incognito = webdriver.ChromeOptions()
-    incognito.add_argument('--incognito')
-    browser = webdriver.Chrome(options=incognito)
-    browser.implicitly_wait(10)
+def browser(request):
+    browser_name = request.config.getoption('browser_name')
+    browser = None
+    if browser_name == 'chrome':
+        print('\nstart browser for testing...')
+        incognito = webdriver.ChromeOptions()
+        incognito.add_argument('--incognito')
+        browser = webdriver.Chrome(options=incognito)
+        browser.implicitly_wait(10)
+    elif browser_name == 'firefox':
+        print('\nstart browser for testing...')
+        browser = webdriver.Firefox()
+    else:
+        raise pytest.UsageError('--browser_name should be chrome or firefox')
     yield browser
     print('\nquit browser...')
     browser.quit()
